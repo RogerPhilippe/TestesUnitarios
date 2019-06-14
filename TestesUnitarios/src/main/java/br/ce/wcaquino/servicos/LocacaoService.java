d9db0cd8cf720340751ae05748aaf804d86ad267
Roger Philippe <rogerphilippe@outlook.com>
2019-06-14 00:04:28 -0300

    Aula 2.2 - Ajuste estrutura classe de teste.

--- TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -31,21 +31,5 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	@Test
-	public void teste(){
-		// Cemário
-		LocacaoService locacaoService = new LocacaoService();
-		Usuario usuario = new Usuario("Usuário 1");
-		Filme filme = new Filme("Filme 1", 2, 5.0);
 
-		// Ação
-		Locacao locacao = locacaoService.alugarFilme(usuario, filme);
-
-		// Verificação
-		Assert.assertTrue(locacao.getValor() == 5.0);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		
-	}
-	
 }
