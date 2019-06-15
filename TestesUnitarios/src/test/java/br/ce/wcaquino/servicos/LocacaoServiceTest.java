243c9fdcaada0d33db55f583efd2c120bd6c61a1
Roger Philippe <rogerphilippe@outlook.com>
2019-06-15 15:18:32 -0300

    AssertThat e precisão assertiva com double.

--- TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ TestesUnitarios/src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -4,16 +4,25 @@ import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
-import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
+
+import static br.ce.wcaquino.utils.DataUtils.obterDataComDiferencaDias;
+/**
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNot.not;
+ **/
+import static org.junit.Assert.*;
 
 public class LocacaoServiceTest {
 
     @Test
-    public void teste(){
-        // Cemário
+    public void testeLocacao(){
+        // Cenário
         LocacaoService locacaoService = new LocacaoService();
         Usuario usuario = new Usuario("Usuário 1");
         Filme filme = new Filme("Filme 1", 2, 5.0);
@@ -22,9 +31,14 @@ public class LocacaoServiceTest {
         Locacao locacao = locacaoService.alugarFilme(usuario, filme);
 
         // Verificação
-        Assert.assertTrue(locacao.getValor() == 5.0);
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+        assertEquals("Valor errado!", locacao.getValor(), 5.0, 0.01);
+        /** Depreciado
+        assertThat(locacao.getValor(), is(5.0));
+        assertThat(locacao.getValor(), is(equalTo(5.0)));
+        assertThat(locacao.getValor(), is(not(5.1)));
+         **/
+        assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)));
 
     }
 
